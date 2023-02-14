66c872a52daa63bbba27f657714ae723ae424866
carlos-paezf <cpaezferreira@gmail.com>
2023-02-14 16:48:44 -0500

    Update Product Migration

--- product/models.py
+++ product/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.conf import settings
 from django.contrib.postgres.fields import ArrayField
 from django.core.validators import MaxValueValidator, MinValueValidator
+from django.utils.text import slugify
 import uuid
 import os
 
@@ -26,7 +27,7 @@ class Product(models.Model):
         MinValueValidator(0)
     ])
     description = models.TextField(max_length=500)
-    slug = models.SlugField(unique=True)
+    slug = models.SlugField(max_length=255, unique=True)
     stock = models.IntegerField(default=0, validators=[
         MinValueValidator(0)
     ])
@@ -35,17 +36,20 @@ class Product(models.Model):
         blank=True,
         default=list
     )
-    rating = models.FloatField(default=5, validators=[
+    rating = models.FloatField(default=3.5, validators=[
         MaxValueValidator(5),
         MinValueValidator(0)
     ])
-    image = models.ImageField(
-        upload_to=product_image_file_path, null=True)
+    image = models.ImageField(upload_to=product_image_file_path, null=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Product, self).save(*args, **kwargs)
 
     class Meta:
         verbose_name = 'product'
